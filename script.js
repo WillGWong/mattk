1f888ad3170b983758a57770f46e824cbd350301
WillGWong <willeyw@gmail.com>
2020-10-01 17:50:53 -0700

    nav buttons works with slides

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
 const carouselImages = document.querySelector('.carousel__images');
 const carouselButtons = document.querySelectorAll('.carousel__button');
-const numberOfImages = document.querySelectorAll('.carousel__images img').length;
+const numberOfImages = document.querySelectorAll('.slide').length;
 let imageIndex = 1;
 let translateX = 0;
 
 carouselButtons.forEach(button => {
   button.addEventListener('click', (event) => {
-    if (event.target.id === 'previous') {
+    if (event.target.id === 'contact') {
       if (imageIndex !== 1) {
         imageIndex--;
-        translateX += 300;
+        translateX += 844;
       }
     } else {
       if (imageIndex !== numberOfImages) {
         imageIndex++;
-        translateX -= 300;
+        translateX -= 844;
       }
     }
     
+    console.log(imageIndex)
     carouselImages.style.transform = `translateX(${translateX}px)`;
   });
 });
+
+function aboutFunction() {
+  if (imageIndex === 2) {
+    imageIndex --;
+    translateX +=844;
+  } else  if (imageIndex === 3) {
+    imageIndex -= 2;
+    translateX += 1688;
+  }
+  console.log(imageIndex)
+  carouselImages.style.transform = `translateX(${translateX}px)`;
+}
+
+function workFunction() {
+  if (imageIndex === 1) {
+    imageIndex ++;
+    translateX -=844;
+  } else  if (imageIndex === 3) {
+    imageIndex --;
+    translateX += 844;
+  }
+  console.log(imageIndex)
+  carouselImages.style.transform = `translateX(${translateX}px)`;
+}
+
+function contactFunction() {
+  if (imageIndex === 1) {
+    imageIndex += 2;
+    translateX -= 1688;
+  } else  if (imageIndex === 2) {
+    imageIndex ++;
+    translateX -= 844;
+  }
+  console.log(imageIndex)
+  carouselImages.style.transform = `translateX(${translateX}px)`;
+}
